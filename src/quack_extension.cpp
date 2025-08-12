f24f3c9dd43ec8e98eac3c623d164cc71a2c9330
Rusty Conover <rusty@conover.me>
2025-08-12 09:14:29 -0400

    fix: initial

--- src/quack_extension.cpp
+++ src/quack_extension.cpp
@@ -7,36 +7,82 @@
 #include "duckdb/function/scalar_function.hpp"
 #include "duckdb/main/extension_util.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
-
-// OpenSSL linked through vcpkg
-#include <openssl/opensslv.h>
+#include <boost/math/distributions.hpp>
 
 namespace duckdb {
 
-inline void QuackScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &name_vector = args.data[0];
-	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "Quack " + name.GetString() + " 🐥");
-	});
+// Template function for normal distribution operations
+template <typename Operation>
+inline void NormalDistributionFunc(DataChunk &args, ExpressionState &state, Vector &result, Operation op) {
+	auto &mean_vector = args.data[0];
+	auto &stddev_vector = args.data[1];
+	auto &x_vector = args.data[2];
+
+	TernaryExecutor::Execute<double, double, double, double>(
+	    mean_vector, stddev_vector, x_vector, result, args.size(), 
+	    [&](double mean, double stddev, double x) {
+		    boost::math::normal_distribution<double> dist(mean, stddev);
+		    return op(dist, x);
+	    });
 }
 
-inline void QuackOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &name_vector = args.data[0];
-	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "Quack " + name.GetString() + ", my linked OpenSSL version is " +
-		                                           OPENSSL_VERSION_TEXT);
-	});
+// Specific function implementations using the template
+inline void NormalPdfFunc(DataChunk &args, ExpressionState &state, Vector &result) {
+	NormalDistributionFunc(args, state, result, 
+	    [](const auto &dist, double x) { return boost::math::pdf(dist, x); });
+}
+
+inline void NormalLogPdfFunc(DataChunk &args, ExpressionState &state, Vector &result) {
+	NormalDistributionFunc(args, state, result, 
+	    [](const auto &dist, double x) { return boost::math::logpdf(dist, x); });
+}
+
+inline void NormalCdfFunc(DataChunk &args, ExpressionState &state, Vector &result) {
+	NormalDistributionFunc(args, state, result, 
+	    [](const auto &dist, double x) { return boost::math::cdf(dist, x); });
+}
+
+inline void NormalLogCdfFunc(DataChunk &args, ExpressionState &state, Vector &result) {
+	NormalDistributionFunc(args, state, result, 
+	    [](const auto &dist, double x) { return boost::math::logcdf(dist, x); });
+}
+
+inline void NormalQuantileFunc(DataChunk &args, ExpressionState &state, Vector &result) {
+	NormalDistributionFunc(args, state, result, 
+	    [](const auto &dist, double p) { return boost::math::quantile(dist, p); });
+}
+
+// Generic template for any distribution with two parameters
+template <typename Distribution, typename Operation>
+inline void TwoParameterDistributionFunc(DataChunk &args, ExpressionState &state, Vector &result, Operation op) {
+	auto &param1_vector = args.data[0];
+	auto &param2_vector = args.data[1];
+	auto &x_vector = args.data[2];
+
+	TernaryExecutor::Execute<double, double, double, double>(
+	    param1_vector, param2_vector, x_vector, result, args.size(), 
+	    [&](double param1, double param2, double x) {
+		    Distribution dist(param1, param2);
+		    return op(dist, x);
+	    });
+}
+
+// Helper function to create a scalar function with consistent signature
+template <typename FuncType>
+ScalarFunction CreateDistributionFunction(const std::string &name, FuncType func) {
+	return ScalarFunction(name, 
+	                     {LogicalType::DOUBLE, LogicalType::DOUBLE, LogicalType::DOUBLE},
+	                     LogicalType::DOUBLE, 
+	                     func);
 }
 
 static void LoadInternal(DatabaseInstance &instance) {
-	// Register a scalar function
-	auto quack_scalar_function = ScalarFunction("quack", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackScalarFun);
-	ExtensionUtil::RegisterFunction(instance, quack_scalar_function);
-
-	// Register another scalar function
-	auto quack_openssl_version_scalar_function = ScalarFunction("quack_openssl_version", {LogicalType::VARCHAR},
-	                                                            LogicalType::VARCHAR, QuackOpenSSLVersionScalarFun);
-	ExtensionUtil::RegisterFunction(instance, quack_openssl_version_scalar_function);
+	// Register normal distribution functions using the template helper
+	ExtensionUtil::RegisterFunction(instance, CreateDistributionFunction("normal_pdf", NormalPdfFunc));
+	ExtensionUtil::RegisterFunction(instance, CreateDistributionFunction("normal_logpdf", NormalLogPdfFunc));
+	ExtensionUtil::RegisterFunction(instance, CreateDistributionFunction("normal_cdf", NormalCdfFunc));
+	ExtensionUtil::RegisterFunction(instance, CreateDistributionFunction("normal_logcdf", NormalLogCdfFunc));
+	ExtensionUtil::RegisterFunction(instance, CreateDistributionFunction("normal_quantile", NormalQuantileFunc));
 }
 
 void QuackExtension::Load(DuckDB &db) {
