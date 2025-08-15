a5bc4047bd90cb9904d97a6d3ab66af78966a407
Rusty Conover <rusty@conover.me>
2025-08-15 18:37:44 -0400

    wip

--- src/quack_extension.cpp
+++ src/quack_extension.cpp
@@ -13,46 +13,50 @@
 
 namespace duckdb {
 
-void Load_chi_squared_distribution(DatabaseInstance &instance);
-void Load_geometric_distribution(DatabaseInstance &instance);
 void Load_bernoulli_distribution(DatabaseInstance &instance);
 void Load_beta_distribution(DatabaseInstance &instance);
-void Load_gamma_distribution(DatabaseInstance &instance);
+void Load_binomial_distribution(DatabaseInstance &instance);
+void Load_chi_squared_distribution(DatabaseInstance &instance);
 void Load_exponential_distribution(DatabaseInstance &instance);
+void Load_extreme_value_distribution(DatabaseInstance &instance);
+void Load_fisher_f_distribution(DatabaseInstance &instance);
+void Load_gamma_distribution(DatabaseInstance &instance);
+void Load_geometric_distribution(DatabaseInstance &instance);
 void Load_laplace_distribution(DatabaseInstance &instance);
+void Load_logistic_distribution(DatabaseInstance &instance);
 void Load_lognormal_distribution(DatabaseInstance &instance);
+void Load_negative_binomial_distribution(DatabaseInstance &instance);
 void Load_normal_distribution(DatabaseInstance &instance);
 void Load_pareto_distribution(DatabaseInstance &instance);
+void Load_poisson_distribution(DatabaseInstance &instance);
+void Load_rayleigh_distribution(DatabaseInstance &instance);
+void Load_students_t_distribution(DatabaseInstance &instance);
 void Load_uniform_int_distribution(DatabaseInstance &instance);
 void Load_uniform_real_distribution(DatabaseInstance &instance);
-void Load_students_t_distribution(DatabaseInstance &instance);
-void Load_fisher_f_distribution(DatabaseInstance &instance);
 void Load_weibull_distribution(DatabaseInstance &instance);
-void Load_extreme_value_distribution(DatabaseInstance &instance);
-void Load_rayleigh_distribution(DatabaseInstance &instance);
-void Load_logistic_distribution(DatabaseInstance &instance);
-void Load_binomial_distribution(DatabaseInstance &instance);
 
 static void LoadInternal(DatabaseInstance &instance) {
 	Load_bernoulli_distribution(instance);
 	Load_beta_distribution(instance);
+	Load_binomial_distribution(instance);
 	Load_chi_squared_distribution(instance);
 	Load_exponential_distribution(instance);
+	Load_extreme_value_distribution(instance);
+	Load_fisher_f_distribution(instance);
 	Load_gamma_distribution(instance);
 	Load_geometric_distribution(instance);
 	Load_laplace_distribution(instance);
+	Load_logistic_distribution(instance);
 	Load_lognormal_distribution(instance);
+	Load_negative_binomial_distribution(instance);
 	Load_normal_distribution(instance);
 	Load_pareto_distribution(instance);
+	Load_poisson_distribution(instance);
+	Load_rayleigh_distribution(instance);
 	Load_students_t_distribution(instance);
 	Load_uniform_int_distribution(instance);
 	Load_uniform_real_distribution(instance);
-	Load_fisher_f_distribution(instance);
 	Load_weibull_distribution(instance);
-	Load_extreme_value_distribution(instance);
-	Load_rayleigh_distribution(instance);
-	Load_logistic_distribution(instance);
-	Load_binomial_distribution(instance);
 }
 
 void QuackExtension::Load(DuckDB &db) {
