c3f3fc65483b426c9a622d408b88c66965ec93b2
Rusty Conover <rusty@conover.me>
2025-12-04 09:39:33 -0500

    fix: build fix

---
+++ src/include/version.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace duckdb {
+
+// Extension version in format YYYYMMDDCC (date + ordinal counter)
+// Update this version when making changes
+constexpr const char *STOCHASTIC_VERSION = "2025120401";
+
+} // namespace duckdb
